2c5a2bf4e5063722497578dda7d9387479f13658
EC2 Default User <ec2-user@ip-172-31-11-210.ap-southeast-2.compute.internal>
2024-02-07 21:44:49 +0000

    memo_typeが1か2かelseの場合のif文を追加

--- memo.rb
+++ memo.rb
@@ -1,8 +1,32 @@
 require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
+puts "文字を入力して下さい"
 
 memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
-# if文を使用して続きを作成していきましょう。
-# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
+  if memo_type == 1
+    puts "新規でメモを作成します　拡張子を除いたファイル名を入力して下さい"
+  
+    file_name = gets.to_s
+    puts "テキストを入力して下さい"
+  
+    memo = STDIN.gets.to_s.chomp
+    CSV.open("#{file_name}.csv" ,"w") do |csv|
+      csv.puts ["#{memo}"]
+  end
+  
+  elsif memo_type == 2
+   puts "既存のメモを編集します　拡張子を除いたファイル名を入力して下さい"
+   
+   file_name = gets.to_s
+   puts "テキストを入力して下さい"
+   
+   memo = STDIN.gets.to_s.chomp
+   CSV.open("#{file_name}.csv" ,"a") do |csv|
+     csv.puts ["#{memo}"]
+  end
+   
+  else
+   puts "半角数字で1または2を入力して下さい"
+  end
